d3a4ae8e8363bb8215eb3ae54c53299bf5f1f453
byronvillalobos <byronvillalobos@hotmail.com>
2021-02-25 03:00:45 +0000

    comments

--- exercises/01.5-building-html-from-arrays/index.js
+++ exercises/01.5-building-html-from-arrays/index.js
@@ -1,8 +1,36 @@
 import React from "react"; //Main React.js library
 import ReactDOM from "react-dom"; //we use ReactDOM to render into the DOM
 
-// only update the value of this array
-const navlinkItems = [];
+// const navlinkItems = [
+// 	<ul className="nav">
+// 		<li className="nav-item">
+// 			<a className="nav-link" href="#">
+// 				Link to google.com
+// 			</a>
+// 		</li>
+// 		<li className="nav-item">
+// 			<a className="nav-link" href="#">
+// 				Link to facebook.com
+// 			</a>
+// 		</li>
+// 		<li className="nav-item">
+// 			<a className="nav-link" href="#">
+// 				Link to amazon.com
+// 			</a>
+// 		</li>
+// 	</ul>
+// ];
+
+const socialnet = ["Link to google.com", "Link to facebook.com", "Link to amazon.com"];
+const navlinkItems = socialnet.map((social, i) => {
+	return (
+		<li className="nav-item" key={i}>
+			<a className="nav-link" href="#">
+				{social}
+			</a>
+		</li>
+	);
+});
 
 const content = <ul className="nav">{navlinkItems}</ul>;
 
