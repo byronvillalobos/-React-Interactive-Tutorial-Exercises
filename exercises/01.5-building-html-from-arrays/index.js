5f0b460262d8eb176db947bb8c66021059c74552
byronvillalobos <byronvillalobos@hotmail.com>
2021-02-25 03:29:13 +0000

    comments

--- exercises/01.5-building-html-from-arrays/index.js
+++ exercises/01.5-building-html-from-arrays/index.js
@@ -1,6 +1,7 @@
 import React from "react"; //Main React.js library
 import ReactDOM from "react-dom"; //we use ReactDOM to render into the DOM
 
+//OPCION1
 // const navlinkItems = [
 // 	<ul className="nav">
 // 		<li className="nav-item">
@@ -21,17 +22,44 @@ import ReactDOM from "react-dom"; //we use ReactDOM to render into the DOM
 // 	</ul>
 // ];
 
-const socialnet = ["Link to google.com", "Link to facebook.com", "Link to amazon.com"];
-const navlinkItems = socialnet.map((social, i) => {
+//OPCION2
+// const socialnet = ["Link to google.com", "Link to facebook.com", "Link to amazon.com"];
+// const navlinkItems = socialnet.map((social, i) => {
+// 	return (
+// 		<li className="nav-item" key={i}>
+// 			<a className="nav-link" href="#">
+// 				{social}
+// 			</a>
+// 		</li>
+// 	);
+// });
+
+//OPCION3
+const socialnet = [
+	{
+		social: "Link to google.com",
+		url: "http://google.com"
+	},
+	{
+		social: "Link to facebook.com",
+		url: "http://facebook.com"
+	},
+	{
+		social: "Link to amazon.com",
+		url: "http://amazon.com"
+	}
+];
+const navlinkItems = socialnet.map(item => {
 	return (
-		<li className="nav-item" key={i}>
-			<a className="nav-link" href="#">
-				{social}
+		<li className="nav-item">
+			<a className="nav-link" href={item.url}>
+				{item.social}
 			</a>
 		</li>
 	);
 });
 
+//final display
 const content = <ul className="nav">{navlinkItems}</ul>;
 
 ReactDOM.render(content, document.querySelector("#myDiv"));
